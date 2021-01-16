c2061dd11bd8479992bf474b8c6f033d28ebb967
mmih123 <77528043+mmih123@users.noreply.github.com>
2021-01-16 12:14:36 +0300

    https://github.com/boo-learn/SpecialistPython2
    Mikhail_M_Update 02_user_base_start.py

--- Module1/practice/user_base/02_user_base_start.py
+++ Module1/practice/user_base/02_user_base_start.py
@@ -1,3 +1,12 @@
+
+NAME = "name"
+SURNAME = "surname"
+USERNAME = "username"
+PASSWORD = "password"
+
+users = {}
+current_id = 0
+
 while True:
     print("Меню")
     print("1. Добавить пользователя")
@@ -12,23 +21,55 @@ while True:
         surname = input("surname: ")
         username = input("username: ")
         password = input("password: ")
-        ...
-        # print(f"Пользователь {surname} {name} добавлен в базу")
+
+        curent_user = {NAME:name, SURNAME:surname, USERNAME:username, PASSWORD:password}
+        current_id += 1
+        users[current_id] = curent_user
+
+        print(f"Пользователь {surname} {name} добавлен в базу")
+
     elif choice == "2":
         print("****Удаление пользователя****")
         id = int(input('id: '))
-        ...
-        # print(f"Пользователь {surname} {name} с id:{id} удален из базы")
-        # print(f"Пользователь с id: {id} не найден")
+
+        curent_user = users.get(id)
+
+        if curent_user == None :
+            print(f"Пользователь с id: {id} не найден")
+
+        else :
+            del users[id]
+            surname = curent_user[SURNAME]
+            name=curent_user[NAME]
+
+            print(f"Пользователь {surname} {name} с id:{id} удален из базы")
 
     elif choice == "3":
         print("****Список всех пользователей****")
-        ...
+
+        for key in users :
+            curent_user = users[key]
+            name = curent_user[NAME]
+            surname = curent_user[SURNAME]
+            username = curent_user[USERNAME]
+
+            print(f"{key}: {name[0]} {surname} [username]")
 
     elif choice == "4":
         print("****Редактирование пользователя****")
         id = int(input('id: '))
-        ...
+
+        curent_user = users.get(id)
+
+        if curent_user == None:
+            print(f"Пользователь с id: {id} не найден")
+            continue
+
+        name = curent_user[NAME]
+        surname = curent_user[SURNAME]
+        username = curent_user[USERNAME]
+        password = curent_user[PASSWORD]
+
         print("Какой параметр хотите изменить?")
         print("1. name")
         print("2. surname")
@@ -37,17 +78,24 @@ while True:
         param_number = input(': ')
         if param_number == '1':
             new_name = input(f"Старое имя {name}. Новое имя: ")
-            ...
+            curent_user[NAME] = new_name
+
         elif param_number == '2':
             new_surname = input(f"Старая фамилия {surname}. Новая фамилия: ")
-            ...
+            curent_user[SURNAME] = new_surname
+
         elif param_number == '3':
-            ...
+            new_username = input(f"Старый username  {username}. Новый username: ")
+            curent_user[USERNAME] = new_username
+
         elif param_number == '4':
-            ...
+            new_password = input(f"Старый праоль  {password}. Новый пароль: ")
+            curent_user[PASSWORD] = password
         else:
-            ...
-        # print(f"Пользователь с id: {id} не найден")
+            continue
+
+        users[id] = curent_user
+
     elif choice == "5":
         print("GoodBy -(")
         break
