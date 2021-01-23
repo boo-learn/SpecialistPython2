f576c27c4d3510efa47538f27783b137d3bb54dc
Paul <37895059+psvtg@users.noreply.github.com>
2021-01-23 17:29:28 +0300

    Update 2_iter_rules.py

--- Module3/examples/2_iter_rules.py
+++ Module3/examples/2_iter_rules.py
@@ -1,33 +1,13 @@
-# ================================
-#    Принципы работы итераторов
-# ================================
+class SuperStr(str):
+    def is_repeatance(self, s):
+        if len(self):
+            if s in self:
+                return True
+        return False
 
-test_list = [1, 2, 3, 4, 'END']
-# Когда вы пишете
-for el in test_list:
-    print(test_list)
+    def is_palindrome(self):
+        return True if self == self[::-1] else False
 
-# Python выполняет:
-# 1. Вызывает метод __iter__(): test_list.__iter__()
-#   Метод __iter__() должен вернуть объект, у которого есть метод __next__()
-# 2. Цикл for..in каждую итерацию вызывает метод __next__()
-#   __next__() при каждом вызове возвращает следующий элемент итератора
-# 3. Когда элементы итератора заканчиваются, метод __next__() возбуждает исключение StopIteration
-#   for..in завершает свою работу, когда перехватывает это исключение
-
-# Проведем обход элементов списка test_list вручную (без цикла)
-
-print("Итерируем вручную...")
-# Получаем объект-итератор
-test_list_iter = iter(test_list)
-# Функция iter() просто вызывает метод __iter__()
-
-print(next(test_list_iter))
-print(next(test_list_iter))
-print(next(test_list_iter))
-print(next(test_list_iter))
-print(next(test_list_iter))
-
-print(next(test_list_iter))  # Будет возбуждено исключение StopIteration
-
+print(SuperStr('abcdecabc').is_repeatance('abc'))
+print(SuperStr('abca').is_palindrome())
 
