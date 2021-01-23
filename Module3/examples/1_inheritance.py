250675f85bbff554914404ba057bced06c1b07d7
konatavi <33349338+Konatavi@users.noreply.github.com>
2021-01-23 12:47:32 +0100

    Update 1_inheritance.py
      ## без целой части пока не работает

--- Module3/examples/1_inheritance.py
+++ Module3/examples/1_inheritance.py
@@ -3,3 +3,6 @@ class Fraction:
         self.int = fr[:fr.find(' ')]
         self.numerator = fr[fr.find(' ') + 1:fr.find('/')]
         self.denominator = fr[fr.find('/') + 1:]
+
+        
+        ## без целой части пока не работает
