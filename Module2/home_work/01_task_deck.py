6e33e925313c11bfba288543900ebdcfc58e3672
Sergey Sitnikov <77528142+svsitnikov@users.noreply.github.com>
2021-01-29 19:28:29 +0300

    Update 01_task_deck.py
    SuperString. Домашнее задание к Модулю 3

--- Module2/home_work/01_task_deck.py
+++ Module2/home_work/01_task_deck.py
@@ -1,2 +1,41 @@
-# Закончите все Задания: с колодой карт из файла practice/03_task_with_deck.md
+class SuperStr(str):
 
+    def is_repeatance(self, s):
+        if not isinstance(s, str):
+            return False
+        n = len(self) // (len(s) or 1)
+        return self == n * s
+
+    def is_palindrom(self):
+        return self.lower() == self.lower()[::-1]
+
+
+mystr = SuperStr('КаЗак')
+print(mystr)
+print(mystr.is_repeatance('за'))
+print(mystr.is_palindrom())
+print('------------------')
+
+mystr = SuperStr('казак')
+print(mystr)
+print(mystr.is_repeatance('за'))
+print(mystr.is_palindrom())
+print('------------------')
+
+mystr = SuperStr('Ляляля')
+print(mystr)
+print(mystr.is_repeatance('ля'))
+print(mystr.is_palindrom())
+print('------------------')
+
+mystr = SuperStr('ляляля')
+print(mystr)
+print(mystr.is_repeatance('ля'))
+print(mystr.is_palindrom())
+print('------------------')
+
+mystr = SuperStr('')
+print(mystr)
+print(mystr.is_repeatance('ля'))
+print(mystr.is_palindrom())
+print('------------------')
