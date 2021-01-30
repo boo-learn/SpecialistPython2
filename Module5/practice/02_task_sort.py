c0d2014bea5c5d206e8a23ec377acc317d2e41bf
Paul <37895059+psvtg@users.noreply.github.com>
2021-01-30 12:15:28 +0300

    Update 02_task_sort.py

--- Module5/practice/02_task_sort.py
+++ Module5/practice/02_task_sort.py
@@ -1,3 +1,20 @@
 # Сумма из диапазона
 # Дан массив(список) чисел.
 # Найти: сумму элементов массива, больших данного числа А, но меньше B.
+from sort_lib import gen_list
+from quick_sort import quick_sort
+
+
+def test2():
+    arr = quick_sort(gen_list(10))
+    A = 3
+    B = 150000
+
+    summ = sum([i for i in arr if B > i > A])
+
+    print(f'МАССИВ: {arr}')
+    print(f'СУММА: {summ}')
+
+    
+if __name__ == '__main__':
+    test2()
