17147b86280db323fe929eae62724b7e032f2617
Rodion4ik <rodion.vlasov@mail.ru>
2021-01-30 10:47:50 +0300

    Update bubble_sort.py

--- Module5/examples/bubble_sort.py
+++ Module5/examples/bubble_sort.py
@@ -16,3 +16,17 @@ while swapped:
             # Устанавливаем swapped в True для следующей итерации
             swapped = True
 print(nums)
+###
+nums = [5, 2, 1, 8, 4]
+swapped = True
+while swapped:
+    swapped = False
+    j=len(nums) - 1
+    for i in range(j):
+        if nums[i] > nums[i + 1]:
+            # Меняем элементы
+            nums[i], nums[i + 1] = nums[i + 1], nums[i]
+            # Устанавливаем swapped в True для следующей итерации
+            swapped = True
+            j-1
+print(nums)
