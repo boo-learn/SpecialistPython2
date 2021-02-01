15b48b9c1e584a7fe2f73f54a20e8ce13baa566e
astaf89 <77969833+astaf89@users.noreply.github.com>
2021-02-01 16:52:56 +0300

    Update 02_deck_start.py

--- Module2/practice/deck/02_deck_start.py
+++ Module2/practice/deck/02_deck_start.py
@@ -1,26 +1,52 @@
+import pprint
+import random
+
 class Card:
+    HEARTS = '\u2665'
+    DIAMONDS = '\u2666'
+    CLUBS = '\u2663'
+    SPADES = '\u2660'
+
     def __init__(self, value, type):
-        pass
+        self.value = value
+        self.type = type
 
     def to_str(self):
-        pass
+         return f"{self.value}{self.type}"
 
 
 class Deck:
     def __init__(self):
         self.cards = []
+        for type in [Card.CLUBS, Card.SPADES, Card.HEARTS, Card.DIAMONDS]:
+            for value in ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'В', 'Д', 'К', 'Т' ]:
+                self.cards.append(Card(value, type))
 
     def show(self):
-        pass
+        print(len(self.cards), 'in deck')
+        for card in self.cards:
+            print(card.to_str())
 
     def draw(self, x):
-        pass
+        i = 0
+        while i != x:
+            print(self.cards[i].to_str())
+            i += 1
 
     def shuffle(self):
-        pass
+        shuffled_deck = []
+        for card in self.cards:
+            shuffled_deck.insert(random.randint(1, 54+1), card)
+        self.cards = shuffled_deck
 
 
+#card1 = Card(3, Card.CLUBS)
+#print(card1)
+#print(card1.to_str())
 deck = Deck()
-print(deck.show())
+deck.show()
+print('=============')
+deck.draw(5)
+print('=============')
 deck.shuffle()
-print(deck.show())
+deck.show()
