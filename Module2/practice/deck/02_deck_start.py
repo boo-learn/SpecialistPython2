71ce185e6592bb1cfa60ff449de97e25eee6dfe6
astaf89 <77969833+astaf89@users.noreply.github.com>
2021-02-01 16:36:44 +0300

    Update 02_deck_start.py

--- Module2/practice/deck/02_deck_start.py
+++ Module2/practice/deck/02_deck_start.py
@@ -1,26 +1,45 @@
+import pprint
+
 class Card:
+    HEARTS = '\u2665'
+    DIAMONDS = '\u2666'
+    CLUBS = '\u2663'
+    SPADES = '\u2660'
+
     def __init__(self, value, type):
-        pass
+        self.value = value
+        self.type = type
 
     def to_str(self):
-        pass
+         return f"{self.value}{self.type}"
 
 
 class Deck:
     def __init__(self):
         self.cards = []
+        for type in [Card.CLUBS, Card.SPADES, Card.HEARTS, Card.DIAMONDS]:
+            for value in ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'В', 'Д', 'К', 'Т' ]:
+                self.cards.append((value + ' ' + type))
 
     def show(self):
-        pass
+        pprint.pprint(self.cards)
 
-    def draw(self, x):
-        pass
+    # def draw(self, x):
+    #     i = 0
+    #     while i != x:
+    #         print(self.__cards[i])
+    #         i += 1
 
-    def shuffle(self):
-        pass
+    # def shuffle(self):
+    #     pass
 
 
+card1 = Card(3, Card.CLUBS)
+print(card1)
+print(card1.to_str())
 deck = Deck()
-print(deck.show())
-deck.shuffle()
-print(deck.show())
+deck.show()
+
+#print(deck.draw(5))
+#deck.shuffle()
+#print(deck.show())
