89c34d8f086c638615b4a4bef84656a1b8dffe16
NikitaPonosov <76036799+NikitaPonosov@users.noreply.github.com>
2021-02-01 16:25:15 +0300

    Update 02_deck_start.py

--- Module2/practice/deck/02_deck_start.py
+++ Module2/practice/deck/02_deck_start.py
@@ -1,26 +1,33 @@
 class Card:
+    SPADES = "\u2660"
+    HEARTS = "\u2665"
+    CLUBS = "\u2663"
+    DIAMONDS = "\u2666"
+    J = 11
+    Q = 12
+    K = 13
+    A = 14
     def __init__(self, value, type):
-        pass
+        self.type = type
+        if value is int:
+            self.value = value
+        if value.lower() == "j":
+            self.value = Card.J
+        if value.lower() == "q":
+            self.value = Card.Q
+        if value.lower() == "k":
+            self.value = Card.K
+        if value.lower() == "a":
+            self.value = Card.A
+
+
 
     def to_str(self):
-        pass
+        return f"{self.value}{self.type}"
+
+    def compare(self):
+
+        # 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A
+        # ♥ > ♦ > ♣ > ♠
 
 
-class Deck:
-    def __init__(self):
-        self.cards = []
-
-    def show(self):
-        pass
-
-    def draw(self, x):
-        pass
-
-    def shuffle(self):
-        pass
-
-
-deck = Deck()
-print(deck.show())
-deck.shuffle()
-print(deck.show())
