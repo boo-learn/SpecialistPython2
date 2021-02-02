4cfefceaf737274eca535fb3a0a224396e0ef41c
astaf89 <77969833+astaf89@users.noreply.github.com>
2021-02-02 16:52:23 +0300

    Update 02_task_Fraction.py

--- Module3/practice/02_task_Fraction.py
+++ Module3/practice/02_task_Fraction.py
@@ -3,16 +3,28 @@
 # Сюда отправляем задание с классом дроби
 
 class Fraction:
-    def __init__(self, fract_str): # Дробь в конструктор передается в виде строки
-        # А мы храним дробь в виде
-        self.numerator = ... # числителя
-        self.denominator = ... # знаменатель
-        # целую часть перебрасываем в числитель
-        # минус, если он есть, тоже храним в числителе
+    def __init__(self, str):
+        self.__number = list(str)
+        if self.__number[0] == '-':
+            self.__number[1] = '-' + self.__number[1]
+            self.__number.pop(0)
+        for el in self.__number:
+            if el in [' ', '/']:
+                self.__number.remove(el)
+        self.__number = list(map(int, self.__number[:]))
 
 
-# Примеры создания дробей:
-fract1 = Fraction("3 12/15")
-fract2 = Fraction("-1 2/6")
-fract3 = Fraction("2/4")
-fract4 = Fraction("-2/4")
+    # кривой косой
+    def __add__(self, other):
+        print((self.__number[0] + self.__number[1] / self.__number[2]) + (other.__number[0] + other.__number[1] / other.__number[2]))
+
+    def show(self):
+        return f"{self.__number[0]} {self.__number[1]}/{self.__number[2]}"
+
+oper1 = Fraction('-2 3/5')
+oper2 = Fraction('4 3/5')
+
+print(oper1.show())
+print(oper2.show())
+
+print(oper1+oper2)
