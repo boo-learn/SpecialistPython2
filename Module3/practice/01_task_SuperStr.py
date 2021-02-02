4019e1c02a50fc7eb9b1df9da98b1eaba9141da0
staf711 <40694301+staf711@users.noreply.github.com>
2021-02-02 15:58:03 +0300

    Update 01_task_SuperStr.py

--- Module3/practice/01_task_SuperStr.py
+++ Module3/practice/01_task_SuperStr.py
@@ -7,3 +7,20 @@
 # 2. метод is_palindrom (), который возвращает True или False в зависимости от того,
 # является ли строка палиндромом. Регистрами символов пренебрегать.
 # Пустую строку считать палиндромом.
+
+
+class SuperStr(str):
+    def __init__(self,str):
+        self.str=str
+    def is_repeatance(self,s):
+        if not isinstance(s,str):
+            return False
+
+    def is_palindrom(self):
+        if self.str == "":
+            return True
+        long_str=len(self.str)//2
+        return self.str[0:long_str] == self.str[long_str+1:]
+
+word=SuperStr('11011')
+print(word.is_palindrom())
