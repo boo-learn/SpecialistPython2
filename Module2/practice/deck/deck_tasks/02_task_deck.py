80f837fc737486f5d0ca7af88ed9a1ddea50431f
romanskarasev <78349699+romanskarasev@users.noreply.github.com>
2021-02-02 13:09:59 +0300

    Update 02_task_deck.py

--- Module2/practice/deck/deck_tasks/02_task_deck.py
+++ Module2/practice/deck/deck_tasks/02_task_deck.py
@@ -1,1 +1,27 @@
 # Сюда отправляем решение второй задачи с колодой
+from classes.Card import Card
+from classes.Deck import Deck
+
+deck = Deck()
+print(deck)
+deck.shuffle()
+print(deck)
+hand = deck.draw(10)
+
+types = [[0, Card.HEARTS],  [0, Card.DIAMONDS], [0, Card.CLUBS], [0, Card.SPADES]]
+
+for card in hand:
+    if card.type == Card.HEARTS:
+        types[0][0] += 1
+    if card.type == Card.DIAMONDS:
+        types[1][0] += 1
+    if card.type == Card.CLUBS:
+        types[2][0] += 1
+    if card.type == Card.SPADES:
+        types[3][0] += 1
+
+types.sort(reverse=1)
+
+print(hand)
+
+print(f"{types[0][1]} - больше")
