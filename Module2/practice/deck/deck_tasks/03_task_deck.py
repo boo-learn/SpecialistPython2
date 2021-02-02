a7d0ff0eeb476866c0da68bae784a44dddcd8f1b
romanskarasev <78349699+romanskarasev@users.noreply.github.com>
2021-02-02 14:03:23 +0300

    Update 03_task_deck.py

--- Module2/practice/deck/deck_tasks/03_task_deck.py
+++ Module2/practice/deck/deck_tasks/03_task_deck.py
@@ -1,1 +1,19 @@
 # Сюда отправляем решение третьей задачи с колодой
+from classes.Card import Card
+from classes.Deck import Deck
+
+deck = Deck()
+print(deck)
+deck.shuffle()
+
+prev_card = deck.draw(1)[0]
+card = None
+
+while True:
+    deck.shuffle()
+    card = deck.draw(1)[0]
+    print(f"Предыдущая карта {prev_card.to_str()}, вытянутая карта {card.to_str()}")
+    if card > prev_card:
+        break
+
+print(f"{card.to_str()} больше {prev_card.to_str()}")
