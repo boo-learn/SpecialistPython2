066ddacf2ad48d6f50e13eb860cda2834598940c
loragirl1164 <57247725+loragirl1164@users.noreply.github.com>
2021-02-02 14:10:36 +0300

    Update 03_task_deck.py
    №3

--- Module2/practice/deck/deck_tasks/03_task_deck.py
+++ Module2/practice/deck/deck_tasks/03_task_deck.py
@@ -1,1 +1,20 @@
 # Сюда отправляем решение третьей задачи с колодой
+from Classes.Deck import Deck
+
+deck=Deck()
+print(f'Исходная колода {deck}')
+deck.shuffle()
+print(f'Первое перемешивание {deck}')
+first_card=deck.draw(1)
+print(f'Первая карта: {first_card}')
+deck.shuffle()
+print(f'Второе перемешивание {deck}')
+second_card=deck.draw(1)
+print(f'Вторая карта: {second_card}')
+
+while first_card>second_card:
+    deck.shuffle()
+    print(f'Следующее перемешивание {deck}')
+    first_card=second_card
+    second_card_card = deck.draw(1)
+    print(f'Следующая карта: {second_card}')
