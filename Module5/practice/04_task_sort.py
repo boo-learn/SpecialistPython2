019d5113008617a6f17321e48f8f9e4e4de1f823
loragirl1164 <57247725+loragirl1164@users.noreply.github.com>
2021-02-03 12:26:37 +0300

    Update 04_task_sort.py

--- Module5/practice/04_task_sort.py
+++ Module5/practice/04_task_sort.py
@@ -1,3 +1,36 @@
+from Gener_list import gen_list
+#from sort_bubble import sort_bubble
+
+
 # Сумма наибольших по модулю
 # Дан массив(список) чисел.
 # Найти: сумму 10-ти самых больших элементов по модулю.
+
+def sort_bubble(nums):
+    swapped = True
+    granica = len(nums) - 1
+    while swapped:
+        swapped = False
+        for i in range(granica):
+            if abs(nums[i]) >abs(nums[i + 1]):
+                # Меняем элементы
+                nums[i], nums[i + 1] = nums[i + 1], nums[i]
+                # Устанавливаем swapped в True для следующей итерации
+                swapped = True
+        granica -= 1
+    return
+
+N=20
+num=gen_list(N,-20,20)
+print('Исходный список: ', num)
+
+sort_bubble(num)
+print('Отсортированный в порядке возрастания модулей: ', num)
+
+if N<10:
+    print('В массиве меньше 10 элементов')
+else:
+    summa=0
+    for i in num[-10:]:
+        summa+=i
+    print(f'Сумма {summa}')
