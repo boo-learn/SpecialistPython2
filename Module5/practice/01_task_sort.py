5c239fa0512f260eb6bfeafab0fc9b1198d1e37b
staf711 <40694301+staf711@users.noreply.github.com>
2021-02-03 11:54:12 +0300

    Update 01_task_sort.py

--- Module5/practice/01_task_sort.py
+++ Module5/practice/01_task_sort.py
@@ -1,3 +1,11 @@
 # Сумма с условием
 # Дан массив(список) чисел.
 # Найти: сумму элементов массива, больших данного числа А
+from gen_list import gen_list
+h=gen_list(10)
+A=10
+sum=0
+for i in h:
+    if i > A:
+        sum+=i
+print(sum)
