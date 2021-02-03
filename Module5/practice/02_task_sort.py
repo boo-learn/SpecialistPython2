781a828726c395b1b7a2e55bcdc3237ac603ec1b
NikitaPonosov <76036799+NikitaPonosov@users.noreply.github.com>
2021-02-03 12:04:03 +0300

    Update 02_task_sort.py

--- Module5/practice/02_task_sort.py
+++ Module5/practice/02_task_sort.py
@@ -1,3 +1,17 @@
 # Сумма из диапазона
 # Дан массив(список) чисел.
 # Найти: сумму элементов массива, больших данного числа А, но меньше B.
+def conditional_sum(list, conda, condb):
+    s = 0
+    for el in list:
+        if el > conda and el < condb:
+            s += el
+    return s
+
+
+list1 = gen_list(10, 0, 10)
+
+print(list1)
+print(conditional_sum(list1, 1, 10))
+print(conditional_sum(list1, 5, 8))
+print(conditional_sum(list1, 10, 1))
