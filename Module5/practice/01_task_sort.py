f01f296f1511e274314f29f86cec0260ea0caf67
astaf89 <77969833+astaf89@users.noreply.github.com>
2021-02-03 11:58:49 +0300

    Update 01_task_sort.py

--- Module5/practice/01_task_sort.py
+++ Module5/practice/01_task_sort.py
@@ -1,3 +1,17 @@
 # Сумма с условием
 # Дан массив(список) чисел.
 # Найти: сумму элементов массива, больших данного числа А
+
+import random
+def gen_list(n, at= -20, till=20):
+    return [random.randint(at, till) for _ in range(n)]
+
+summa = 0
+a = 2
+array = gen_list(10)
+for el in array:
+    if el > a:
+        summa += el
+
+print(array)
+print(summa)
