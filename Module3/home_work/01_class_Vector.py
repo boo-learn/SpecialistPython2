e8ad3a5873931db693f1569e53f33b963c23975e
ChekLub <76036839+ChekLub@users.noreply.github.com>
2021-02-03 10:02:37 +0300

    Update 01_class_Vector.py

--- Module3/home_work/01_class_Vector.py
+++ Module3/home_work/01_class_Vector.py
@@ -1,1 +1,43 @@
-# Сюда отправляем решение задачи "Вектор"
+class Vector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, vector):
+        res_x = self.x + vector.x
+        res_y = self.y + vector.y
+        res_vector = Vector(res_x, res_y)
+        return res_vector
+
+    def __sub__(self, vector):
+        res_x = self.x - vector.x
+        res_y = self.y - vector.y
+        res_vector = Vector(res_x, res_y)
+        return res_vector
+
+    def __mul__(self, scalar):
+        res_x = self.x * scalar
+        res_y = self.y * scalar
+        res_vector = Vector(res_x, res_y)
+        return res_vector
+
+    def __rmul__(self, scalar):
+        res_x = self.x * scalar
+        res_y = self.y * scalar
+        res_vector = Vector(res_x, res_y)
+        return res_vector
+
+    def __repr__(self):
+        return f'({self.x}, {self.y})'
+
+v1 = Vector(1,2)
+v2 = Vector(1,2)
+v3 = v1 + v2
+v4 = v1 - v2
+scalar = 6
+v5 = v1 * scalar
+print(f" v1: {v1}")
+print(f" v2: {v2}")
+print(f" v3 = v1 + v2: {v3}")
+print(f" v4 = v1 - v2: {v4}")
+print(f" v5 = v1 * {scalar}: {v5}")
