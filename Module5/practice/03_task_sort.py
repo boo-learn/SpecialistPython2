60b2be8c3991629c091bf5ecc94bfcfecf9e43b5
Dmitry.N <77688182+DrunkSpacer@users.noreply.github.com>
2021-02-03 12:12:03 +0300

    Update 03_task_sort.py

--- Module5/practice/03_task_sort.py
+++ Module5/practice/03_task_sort.py
@@ -1,3 +1,20 @@
 # Сумма наибольших
 # Дан массив(список) чисел.
 # Найти: сумму 10-ти самых больших элементов
+
+from gen_list import gen_list
+from bubble_sort import bubble_sort
+
+
+nums = gen_list(3)
+bubble_sort(nums)
+sum = 0
+if len(nums) <= 10:
+    sum = sum(nums)
+else:
+    i = len(nums) - 10
+    for i in range(len(nums)):
+        sum += nums[i]
+
+print(nums)
+print(sum)
