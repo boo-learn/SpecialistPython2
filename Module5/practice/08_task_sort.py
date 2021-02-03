d7d740a8898e287580f4d1537218be6b7b4ed368
staf711 <40694301+staf711@users.noreply.github.com>
2021-02-03 13:55:55 +0300

    Update 08_task_sort.py

--- Module5/practice/08_task_sort.py
+++ Module5/practice/08_task_sort.py
@@ -3,3 +3,13 @@
 # Телефон задан в виде строки формата nn-nn-nn. Например, 23-45-67
 
 phones = ["25-17-58", "11-34-85", "54-61-56", "34-61-72", "25-17-55", "34-56-56"]
+from bubble_sort import sort
+new_list=[]
+new_phones=[]
+for i in phones:
+    new_list.append(i.split("-"))
+# new_list.sort()
+sort(new_list)
+for i in new_list:
+    new_phones.append('-'.join(i))
+print(new_phones)
