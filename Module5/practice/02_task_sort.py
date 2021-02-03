32272c7f67d11207e2eae203f06e6ac42428f175
romanskarasev <78349699+romanskarasev@users.noreply.github.com>
2021-02-03 12:07:49 +0300

    Update 02_task_sort.py

--- Module5/practice/02_task_sort.py
+++ Module5/practice/02_task_sort.py
@@ -1,3 +1,19 @@
 # Сумма из диапазона
 # Дан массив(список) чисел.
 # Найти: сумму элементов массива, больших данного числа А, но меньше B.
+from gen_list import gen_list
+
+def get_summ(nums, A, B):
+    if A > B:
+        print("некорректные границы")
+        return
+    j = len(nums)
+    sum = 0
+    for i in range(j):
+        if nums[i] > A and nums[i] < B:
+            sum += nums[i]
+    return sum
+
+nums = gen_list(10)
+print(nums)
+print(get_summ(nums, 60, 40))
