f8ef0705b079791daabc72008d9fe09f611d9a83
staf711 <40694301+staf711@users.noreply.github.com>
2021-02-03 12:05:31 +0300

    Update 05_task_sort.py

--- Module5/practice/05_task_sort.py
+++ Module5/practice/05_task_sort.py
@@ -1,3 +1,10 @@
 # Анализ алгоритма сортировки “пузырьком”
 # Массив(список) отсортирован алгоритмом “пузырька”.
 # Определите, сколько обменов сделает алгоритм по возрастанию.
+from gen_list import gen_list
+from bubble_sort import sort
+h=gen_list(100)
+print(h)
+j=sort(h)
+print(j)
+print(h)
