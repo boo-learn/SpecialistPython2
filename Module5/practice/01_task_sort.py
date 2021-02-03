d742ae118fabd25011c06fa0ed9dc367cf60b768
NikitaPonosov <76036799+NikitaPonosov@users.noreply.github.com>
2021-02-03 12:00:26 +0300

    Update 01_task_sort.py

--- Module5/practice/01_task_sort.py
+++ Module5/practice/01_task_sort.py
@@ -1,3 +1,17 @@
 # Сумма с условием
 # Дан массив(список) чисел.
 # Найти: сумму элементов массива, больших данного числа А
+def conditional_sum(list, cond):
+    s = 0
+    for el in list:
+        if el > cond:
+            s += el
+    return s
+
+
+list1 = gen_list(10, 0, 10)
+
+print(list1)
+print(conditional_sum(list1, 1))
+print(conditional_sum(list1, 5))
+print(conditional_sum(list1, 10))
