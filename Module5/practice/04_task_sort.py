07d4c8420f96137b4a1b50caea4900116b521229
staf711 <40694301+staf711@users.noreply.github.com>
2021-02-03 12:02:57 +0300

    Update 04_task_sort.py

--- Module5/practice/04_task_sort.py
+++ Module5/practice/04_task_sort.py
@@ -1,3 +1,11 @@
 # Сумма наибольших по модулю
 # Дан массив(список) чисел.
 # Найти: сумму 10-ти самых больших элементов по модулю.
+from gen_list import gen_list
+from bubble_sort import sort
+h=gen_list(100)
+sort(h)
+sum=0
+for i in h[-10:]:
+    sum+=abs(i)
+print(sum)
