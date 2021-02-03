749647ccc8ccb2a8a9c9b7dcfa2b9d7ff07ffb50
astaf89 <77969833+astaf89@users.noreply.github.com>
2021-02-03 12:07:13 +0300

    Update 03_task_sort.py

--- Module5/practice/03_task_sort.py
+++ Module5/practice/03_task_sort.py
@@ -1,3 +1,15 @@
 # Сумма наибольших
 # Дан массив(список) чисел.
 # Найти: сумму 10-ти самых больших элементов
+import random
+from bubble import bubble
+
+def gen_list(n, at= -20, till=20):
+    return [random.randint(at, till) for _ in range(n)]
+
+array = gen_list(20)
+print(array)
+bubble(array)
+
+print(array[:10:-1])
+print(sum(array[:10:-1]))
