e1e2d3c1ff712f1dde313c47c1a471ab65dbec7c
staf711 <40694301+staf711@users.noreply.github.com>
2021-02-03 12:23:11 +0300

    Update 06_task_sort.py

--- Module5/practice/06_task_sort.py
+++ Module5/practice/06_task_sort.py
@@ -11,3 +11,11 @@
 # 10 1 3 4 3 5 6 7 7 6 1
 # Выход
 # 5
+from gen_list import gen_list
+from bubble_sort import sort
+h=gen_list(99)
+sort(h,order='decrease')
+awardee=[]
+for i in h:
+    awardee.append([f'{i}',h.count(i)])
+print(awardee[:3])
