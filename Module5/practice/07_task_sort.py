a710de9907230399faa90b59b18bcca830bec9dd
akislekov <77528004+akislekov@users.noreply.github.com>
2021-02-04 20:23:37 +0300

    Update 07_task_sort.py

--- Module5/practice/07_task_sort.py
+++ Module5/practice/07_task_sort.py
@@ -13,3 +13,27 @@
 # 70
 # Пояснение:
 # Возможен такой порядок: 10 2 50 1 10
+goods = [5,2,1,10,50,10,4]
+
+goods1 = sorted(goods)
+goods2 = sorted(goods,reverse=True)
+
+print(goods2)
+print(goods1[0:len(goods)//2])
+
+goods3 = goods1[0:len(goods)//2]
+
+
+lall = len(goods2)
+lfree= len(goods1[0:len(goods)//2])
+
+print(  goods2[0:lall - lfree])
+
+res = []
+for i in range(0,lall - lfree):
+    res.append(goods2[i])
+    if i < lfree:
+        res.append(goods3[i])
+
+print(res)
+print(sum(goods2[0:lall - lfree]))
