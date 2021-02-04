f0ce613e7885990cb6e3de879d9f136b54b9bd4a
Sergey Sitnikov <77528142+svsitnikov@users.noreply.github.com>
2021-02-04 21:58:39 +0300

    Update 01_task_sort.py
    Домашнее задание 3

--- Module5/practice/01_task_sort.py
+++ Module5/practice/01_task_sort.py
@@ -1,3 +1,25 @@
-# Сумма с условием
-# Дан массив(список) чисел.
-# Найти: сумму элементов массива, больших данного числа А
+# Дан массив(список) целых чисел.
+# Определить, какое число в массиве встречается чаще всего.
+# Вывести это число и количество повторений.
+
+def gen_list(size, at=-100, to=100):
+    import random
+    return [random.randint(at, to) for _ in range(size)]
+
+
+arr = gen_list(20, at=0, to=20)
+print(f'Массив {arr}')
+arr_to_set = set(arr)  # Получаем множество уникальных членов массива
+most_common = None
+qty_most_common = 0
+
+for item in arr_to_set:
+    qty = arr.count(item)  # Получаем число вхождений значения в массиве
+    if qty > qty_most_common:
+        qty_most_common = qty
+        most_common = item
+
+if qty_most_common > 1:
+    print(f'Чаще всего - {qty_most_common} раз(а) встречается число {most_common}')
+else:
+    print('Члены массива не повторяются')
