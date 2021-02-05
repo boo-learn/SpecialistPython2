7c3cad427bc92e905cea76c74861b915483480b0
Lubl-42 <77528113+Lubl-42@users.noreply.github.com>
2021-02-05 11:23:20 +0300

    Update 10_task_sort.py

--- Module5/practice/10_task_sort.py
+++ Module5/practice/10_task_sort.py
@@ -1,6 +1,25 @@
-# Камни*
-# Имеется N камней веса А1,А2,...,АN.
-#
-# Необходимо разбить их на две кучи таким образом, чтобы веса куч отличались не более чем в 2 раза.
-# Если этого сделать нельзя, то указать это.
+# задание по поиску наиболее часто встречающегося числа из списка
+def gen_list(size, at=-100, to=100):
+    import random
+    return [random.randint(at, to) for _ in range(size)]
 
+
+def frex_dict(mas):
+    dict = {}
+    for di in d:
+        dict.update({di: d.count(di)})
+    return dict
+
+def max_frex(dict):
+    max_dict={}
+    for i in list(dict.keys()):
+        if dict.get(i) == max(list(dict.values())):
+            max_dict.update({i:dict.get(i)})
+    return max_dict
+
+#d = [1, 2, 4, 5, 2, 5, 1, 4, 1, 3, 2, 3]
+d=gen_list(100,-5,5)
+frex_d= frex_dict(d)
+#print(d)
+#print(frex_d)
+print (max_frex(frex_d))
