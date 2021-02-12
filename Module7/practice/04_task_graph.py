ab751a5fe2891b9392e1adb7e1c6b750ed70fee3
akislekov <77528004+akislekov@users.noreply.github.com>
2021-02-12 15:59:10 +0300

    Update 04_task_graph.py

--- Module7/practice/04_task_graph.py
+++ Module7/practice/04_task_graph.py
@@ -1,16 +1,78 @@
-# "Друзья друзей"
-# У всех людей, по крайне мере я надеюсь, есть друзья. У ваших друзей тоже есть друзья и так далее.
-# Вы решили запустить свой бизнес и пригласить максимальное количество людей на его открытие.
-# Вам в руки попала, как нельзя кстати, база людей со списком их друзей.
-# Считаем, что комбинация Имя+Фамилия нам позволяет однозначно идентифицировать человека.
+import json #подключили библиотеку для работы с json
+ 
+dict = {}
+graph = []
+list = []
 
-# Задача
-# По предоставленным данным(файл peoples.json) определите:
-# 1. Сколько людей придет на открытие, если вы отправляете приглашение конкретному человеку
-# (любому, на ваш выбор, из базы), а тот всем друзьям, друзья друзьям и т.д.
+with open(r'c:\temp\peoples.json', "r", encoding = "utf-16") as read_file:
+    data = json.load(read_file)
+    read_file.close()
+    
 
-# 2. Какому минимальному числу людей, нужно отправить приглашение,
-# чтобы пришли ВСЕ люди, присутствующие в базе?
+for txt in data: #создали цикл, который будет работать построчно
+        list = []
+        dict[txt['name'] + ' ' + txt['surname']] = 0
+        print(txt['name'], ':' , txt['surname'])
+
+        for txt2 in txt['friends']:
+            print('--------',txt2['name'], ':' , txt2['surname'])
+            flag = 0
+            for name1, ver1 in dict.items():
+                if txt2['name'] + ' ' + txt2['surname'] == name1:flag = 1
+            if flag == 0: dict[txt2['name'] + ' ' + txt2['surname']] = 0
+            list.append(txt2['name'] + ' ' + txt2['surname'])
+            
+        dict[txt['name'] + ' ' + txt['surname']] = list
+
+listname = []
+
+for name, ver in dict.items():
+
+        listname.append(name)
+        print(name)
+        
+        if ver == 0:
+            list = []
+        else:
+            list = ver
+            
+        for name1, ver1 in dict.items():
+            if ver1 != 0 and name in ver1:
+                list.append(name1)
+        dict[name] = list
+
+graph = []
+
+for name2, ver2 in dict.items():
+    
+    otvet = []
+    
+    print(name2,':',ver2)
+    
+    for c in ver2:
+        if c in listname:
+            i = listname.index(c)
+            otvet.append(i)
+
+    graph.append(otvet)
 
 
-# Сюда отправляем полное решение
+def dfs(start_point, graph):
+    visited = [False] * (len(graph))
+    def _dfs(v):
+        visited[v] = True
+        for w in graph[v]:
+            if not visited[w]:  # посещён ли текущий сосед?
+                _dfs(w)
+    _dfs(start_point)
+    return visited
+
+a=str( input('Введит ФИО: ') )
+
+visited = dfs(listname.index(c), graph)
+
+k = 1
+for x in visited:
+    if x: k += 1
+    
+print(f'Придет {k} человек')    
